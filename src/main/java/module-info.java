7cdb1689c91801e289c4489d5a254d91a507ce1f
zhux2 <3022692203@qq.com>
2024-11-21 22:52:14 +0800

    [demo] - update gradle config

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,6 +11,9 @@ module com.todolist.todo {
 
     opens com.todolist.todo to javafx.fxml;
     opens com.todolist.todo.Controller to javafx.fxml;
+    opens com.todolist.todo.Controller.ViewCtrl to javafx.fxml;
+    opens com.todolist.todo.Controller.CellCtrl to javafx.fxml;
+    opens com.todolist.todo.Controller.PaneCtrl to javafx.fxml;
     exports com.todolist.todo;
     exports com.todolist.todo.Controller;
     exports com.todolist.todo.Controller.ViewCtrl;
